a8a6a7025f9b5aacdc4594671be8d263b64f3992
Cory Price <cprice@mozilla.com>
2016-04-09 07:59:42 -0700

    Complete front and backend

--- app/main.jsx
+++ app/main.jsx
@@ -1,16 +1,14 @@
-//main.jsx
 var React = require("react");
 var ReactDOM = require("react-dom");
-var SchoolList = require("./components/SchoolList.jsx");
+var SchoolsList = require("./components/SchoolList.jsx");
 var schoolsStore = require("./stores/schoolsStore");
-var _schools = schoolsStore.getSchools();
-schoolsStore.onChange(function(schools){
+var _schools = [];
+var getSchoolsCallback = function(schools){
     _schools = schools;
     render();
-});
+};
+schoolsStore.onChange(getSchoolsCallback);
 
 function render(){
-    ReactDOM.render(<SchoolList schools={_schools} />, document.getElementById("container"));    
+    ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
 }
-
-render();
