68eec3eddd613cfd9600095cced61d554120b75d
Cory Price <cprice@mozilla.com>
2016-04-09 07:47:43 -0700

    Start on backend

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 Create API endpoints on the fly for testing, public use, and whatever else.
 
 ## Development
+
+### Front end
 - `git clone https://github.com/ckprice/apirabbit.git`
 - `cd apirabbit`
 - `npm install -g gulp bower nodemon`
 - `gulp` (have to run this after any changes)
 - `nodemon ./server/server.js`
 - Browse to [`http://localhost:7777/index.html`](http://localhost:7777/index.html)
+
+### Database - configure mongo
+- `sudo mkdir -p /data/db`
+- `sudo mongod` - start the database (you'll have to make sure its running)
+
+## Architecture Notes
+
+`Views` => `Actions` => `Dispatcher` => `Store`
+
+## Database
+
--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,16 @@
+//main.jsx
 var React = require("react");
 var ReactDOM = require("react-dom");
-var SchoolsList = require("./components/SchoolList.jsx");
+var SchoolList = require("./components/SchoolList.jsx");
+var schoolsStore = require("./stores/schoolsStore");
+var _schools = schoolsStore.getSchools();
+schoolsStore.onChange(function(schools){
+    _schools = schools;
+    render();
+});
 
-var _schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
-                {name:"Bishop",tagline:"Another great school"}];
-                
 function render(){
-    ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
+    ReactDOM.render(<SchoolList schools={_schools} />, document.getElementById("container"));    
 }
+
 render();
